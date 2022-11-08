0dbea847041bcfa913d056bd342a71ab3e1db339
eddie <edvin.fajkovic3@gmail.com>
2022-11-08 13:14:13 +0000

    test update

--- app/models.py
+++ app/models.py
@@ -33,18 +33,30 @@ class User(UserMixin, db.Model):
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def follow(self, user):
+        """
+        follow
+        """
         if not self.is_following(user):
             self.followed.append(user)
 
     def unfollow(self, user):
+        """
+        unfollow
+        """
         if self.is_following(user):
             self.followed.remove(user)
 
     def is_following(self, user):
+        """
+        is_following
+        """
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
 
     def followed_posts(self):
+        """
+        followed_posts
+        """
         followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
                 followers.c.follower_id == self.id)
@@ -54,6 +66,9 @@ class User(UserMixin, db.Model):
 
 
     def __repr__(self):
+        """
+        __repr__
+        """
         return f'<User {self.username}, {self.email}>'
 
     def set_password(self, password):
